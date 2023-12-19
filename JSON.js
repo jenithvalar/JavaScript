8ab7b6bb69370bee75c16398379a750eb4c2a387
jenith varma <89399356+jenithvalar@users.noreply.github.com>
2023-12-19 11:04:29 +0530

    JavaScript

---
+++ JSON.js
@@ -0,0 +1,15 @@
+let json = () => {
+    let jsonString = '{"key": "sum of two digit"}';
+    let jsonObject = JSON.parse(jsonString);
+    console.log(jsonObject);
+    console.log(typeof(jsonObject))
+    console.log(typeof(jsonString))
+};
+
+json();
+
+const obj = {name: "John", age: 30, city: "New York"};
+let string = JSON.stringify(obj)
+console.log(string)
+console.log(typeof(obj))
+console.log(typeof(string))
