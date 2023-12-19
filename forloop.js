886e77a8501500947cc5a263e82a33d87e570950
jenith varma <89399356+jenithvalar@users.noreply.github.com>
2023-12-19 11:27:54 +0530

    forloop.js

--- forloop.js
+++ forloop.js
@@ -30,15 +30,15 @@
 // console.log(nums)
 // console.table(nums)
 
-let names = ["jeni","valar", "sakthi","Ram","Bairavi"];
-for (let i=0; i<names.length;i++){
-    console.log(names[i])
-}
+// let names = ["jeni","valar", "sakthi","Ram","Bairavi"];
+// for (let i=0; i<names.length;i++){
+//     console.log(names[i])
+// }
 
-//for off loop
-for (let name of names){
-    console.log(name)
-}
+// //for off loop
+// for (let name of names){
+//     console.log(name)
+// }
 // for in loop-we can use to loop the objects
 // let user = {
 //     name:"jenith",
@@ -53,3 +53,43 @@ for (let name of names){
 // console.log(keys)
 // let value=Object.values(user);
 // console.log(value)
+
+// let b = [1,2,3,4,5,6,7]
+
+
+// for(let i=1; i<=b.length;i++){
+//     console.log(i)
+// }
+
+// for(let value of b){
+//     console.log(value)
+// }
+
+// for (let index in b) {
+//     console.log(index);
+// }
+
+
+
+
+// let a = {"name":"jeni","age":23,"place":"coimbatore"}
+// for (let index in a) {
+//     console.log(index+"  "+a[index]);
+// }
+
+
+// let c = "The person";
+// let count = 5
+// for(let value of c){
+//     console.log(value)
+// }
+
+// for(let i=0;i<count;i++){
+//     for(let value of c){
+//         console.log(value)
+//     }
+// }
+
+// for(let i=0;i<c.length;i++){
+//     console.log(c[i])
+// }
