d0907dea87c0d0924bab551fbe7d08c98edaacf4
jenith varma <89399356+jenithvalar@users.noreply.github.com>
2023-12-20 14:16:42 +0530

    promises.js

--- promises.js
+++ promises.js
@@ -49,39 +49,4 @@ let order = (time,work)=>{
 
     })
 };
-order(2000,()=>console.log(`${stocks.Fruits[0]} was selected`))
 
-.then(()=>{
-    return order(0, ()=>console.log("production has started"))
-
-})
-.then(()=>{
-    return order(2000,()=>console.log("The fruit has been chopped"))
-    
-})
-.then(()=>{
-    return order(1000,()=>console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`))
-    
-})
-.then(()=>{
-    return order(1000,()=> console.log("The machine has started"))
-    
-})
-.then(()=>{
-    return order(2000,()=>console.log(`Icecream was placed on ${stocks.holder[0]}`))
-    
-})
-.then(()=>{
-    return order(3000,()=>console.log(`${stocks.toppings[0]} was added as toppings`))
-    
-})
-.then(()=>{
-    return order(2000,()=> console.log("Serve the ice cream"))
-    
-})
-.catch(()=>{
-    console.log("customer left")
-})
-.finally(()=>{
-    console.log("Day ended the shop is closed")
-})
