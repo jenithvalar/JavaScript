2e07e37e83601d47dbaa9409480bf312aa61ca6c
jenith varma <89399356+jenithvalar@users.noreply.github.com>
2024-01-01 10:22:37 +0530

    prototype.js

--- prototype.js
+++ prototype.js
@@ -7,32 +7,32 @@
 
 // proto.prototype.age2=30; /// the data to access with the all objects
 
-// pro=new proto();
-// console.log(pro.name);
-// console.log(pro.gender);
+pro=new proto();
+console.log(pro.name);
+console.log(pro.gender);
 
-// pro.age=35;
+pro.age=30;
 
-// console.log(pro.age2)
+console.log(pro.age2)
 
-// pro1=new proto();
-// console.log(pro1.name);
-// console.log(pro1.gender);
-// console.log(pro1.age2)
+pro1=new proto();
+console.log(pro1.name);
+console.log(pro1.gender);
+console.log(pro1.age2)
 
 //---------------------------------------------------------------
 
-// class proto{
-//     constructor(id,name){
-//         this.id=id;
-//         this.name=name;
-//     }
-// }
+class proto{
+    constructor(id,name){
+        this.id=id;
+        this.name=name;
+    }
+}
 
-// proto.prototype.age=24;
+proto.prototype.age=24;
 
-// pro=new proto(101,"jeni");
-// console.log(pro.id,pro.name,pro.age);
+pro=new proto(101,"jeni");
+console.log(pro.id,pro.name,pro.age);
 //----------------------------------------
 // class proto{
 //     constructor(id,name){
